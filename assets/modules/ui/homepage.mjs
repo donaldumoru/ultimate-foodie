fc62ba7af5c4f57991303a4eda5a78c0945038d1
donaldumoru <donaldumoru101@gmail.com>
2025-09-11 11:20:28 +0200

    testing----main

---
+++ assets/modules/ui/homepage.mjs
@@ -0,0 +1,26 @@
+const homepageDataFile = '/data/homepage.json';
+
+const fetchHomepageData = async function (data) {
+  try {
+    const response = await fetch(data);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const homepageData = await fetchHomepageData(homepageDataFile);
+
+const MAKE_MAIN = function (data) {
+  const heroH1 = document.querySelector('h1');
+  console.log(heroH1);
+  heroH1.textContent = data.herosection.title;
+};
+
+MAKE_MAIN(homepageData);
